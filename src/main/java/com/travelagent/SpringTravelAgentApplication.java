9e9b363e21b55a6296575f6810b7388fbbcb5bab
TrongTai <nguyentrongtai.ait@gmail.com>
2022-09-24 16:49:45 +0700

    Initial commit

---
+++ src/main/java/com/travelagent/SpringTravelAgentApplication.java
@@ -0,0 +1,13 @@
+package com.travelagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTravelAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTravelAgentApplication.class, args);
+	}
+
+}
