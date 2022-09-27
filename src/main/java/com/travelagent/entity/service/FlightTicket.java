512e36bf7771abaf348b226289d7147ce3eab083
TrongTai <nguyentrongtai.ait@gmail.com>
2022-09-27 21:55:30 +0700

    fix lombok annotation in service classes

--- src/main/java/com/travelagent/entity/service/FlightTicket.java
+++ src/main/java/com/travelagent/entity/service/FlightTicket.java
@@ -1,22 +1,16 @@
 package com.travelagent.entity.service;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @SuppressWarnings("serial")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "flight_tickets")
-public class FlightTicket extends Service implements Serializable {
+public class FlightTicket extends Service {
 
 }
--- src/main/java/com/travelagent/entity/service/Tour.java
+++ src/main/java/com/travelagent/entity/service/Tour.java
@@ -1,22 +1,16 @@
 package com.travelagent.entity.service;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @SuppressWarnings("serial")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tours")
-public class Tour extends Service implements Serializable {
+public class Tour extends Service {
 
 }
